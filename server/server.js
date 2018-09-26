0308c94e7b90f07be9d7666639c07e3be1ef46bc
kirkol <antonina.kirkalo-kowerczyk@infopark.de>
2018-09-26 14:44:03 +0200

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
+
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 const app = express()
@@ -14,34 +16,22 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected')
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  })
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  })
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
     //za pomoca linii ponizej powodujemy, ze wiadomosci wyslane przez nas samych
     //trafia do wszystkich Z WYJATKIEM nas samych
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    })
-    
-    //wyemitowanie eventu do wszystkich polaczen (np. wielu klientow jednoczesnie)
-    //tu: jesli jeden klient wysle wiadomosc na serwer, to dzieki linii nizej dostana ja wszyscy
-    // io.emit('newMessage', {
+    // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // })
+    
+    //wyemitowanie eventu do wszystkich polaczen (np. wielu klientow jednoczesnie)
+    //tu: jesli jeden klient wysle wiadomosc na serwer, to dzieki linii nizej dostana ja wszyscy
+    io.emit('newMessage', generateMessage(message.from, message.text))
   })
 
   socket.on('disconnect', () => {
