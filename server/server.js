cfdd4ac30afb8d80ed71cf126cad014100ca1bdb
kirkol <antonina.kirkalo-kowerczyk@infopark.de>
2018-09-26 09:50:08 +0200

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 //pobranie sciezki do folderu public
 const publicPath = path.join(__dirname, '../public')
@@ -7,12 +9,31 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+//stworzenie serwera na podstawie expressa (tez serwera;)), ktory bedzie uzywal biblioteki http
+const server = http.createServer(app)
+
+//stworzenie serwera, ktory bedzie obslugiwal sockety
+//bedzie nasluchiwal eventow ze strony usera
+//lub sam je bedzie wywolywal
+let io = socketIO(server)
+
 //ustawienie middleware'a expressa
 //express jako server udostepni katalog publiczny (ponizej ustawiamy jego sciezke)
 app.use(express.static(publicPath))
 
-//usatwienie nasluchu na porcie 3000 (na razie deweloperski tylko)
-app.listen(port, () => {
+//tworzymy listenera eventow. Mozemy nasluchiwac konkretnych eventow od strony klienta
+//i ofc reagowac na te eventy
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  //listener nasluchujacy strony klienta - jesli polaczenie padnie od strony klienta,
+  //to odpala sie kod listenera ;) 
+  socket.on('disconnect', () => {
+    console.log('User has been disconnected')
+  })
+})
+
+server.listen(port, () => {
   console.log(`Started up at port ${port}`)
 })
 
