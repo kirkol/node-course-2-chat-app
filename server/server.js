7fe9d8068a61a3da7dd625f347bb576b65c51586
kirkol <antonina.kirkalo-kowerczyk@infopark.de>
2018-09-26 14:18:12 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,15 +14,34 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
-    //wyemitowanie eventu do wszystkich polaczen (np. wielu klientow jednoczesnie)
-    //tu: jesli jeden klient wysle wiadomosc na serwer, to dzieki linii nizej dostana ja wszyscy
-    io.emit('newMessage', {
+    //za pomoca linii ponizej powodujemy, ze wiadomosci wyslane przez nas samych
+    //trafia do wszystkich Z WYJATKIEM nas samych
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     })
+    
+    //wyemitowanie eventu do wszystkich polaczen (np. wielu klientow jednoczesnie)
+    //tu: jesli jeden klient wysle wiadomosc na serwer, to dzieki linii nizej dostana ja wszyscy
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
